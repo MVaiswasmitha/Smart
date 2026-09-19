f84e55b6d855ee80fc662eb47066c1308decf9a9
MVaiswasmitha <mvaiswasmitha@gmail.com>
2026-09-19 16:19:52 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,17 @@
+// KisanCare Service Worker - Cache reset and network pass-through
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Always fetch fresh from network
+  event.respondWith(fetch(event.request));
+});
